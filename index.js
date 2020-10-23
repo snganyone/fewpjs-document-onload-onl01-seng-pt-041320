74ba22f02f19934a8840e9c21cc30510fc1cc198
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-10-22 20:12:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+let p = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+    p.innerHTML = "This is really cool!";
+});
+
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
